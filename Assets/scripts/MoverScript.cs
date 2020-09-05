d33bfbd1ecfc34c3c12fd035f5e5bb90544f9cb7
Parker <10714265@my.uvu.edu>
2020-09-04 21:19:22 -0600

    pls hlp
    pls help

---
+++ Assets/scripts/MoverScript.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    public float speed = 5f;
+    public int score = 100;
+
+    public float health = 100;
+
+    private void Update()
+    {
+        var myInput = speed * Time.deltaTime * Input.GetAxis("Vertical");
+        var urInput = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        
+        transform.Translate(myInput, urInput, 0);
+    }
+    
+}
